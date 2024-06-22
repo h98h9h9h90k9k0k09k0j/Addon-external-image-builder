58210b7ad092b8daa96ce5a15cf0b87a672b80fa
AleksanderRDL <55142567+AleksanderRDL@users.noreply.github.com>
2024-06-22 20:44:49 +0200

    Fix path

--- run.sh
+++ run.sh
@@ -5,9 +5,9 @@ source /opt/venv/bin/activate
 echo "activated virtual environment"
 
 # Generate the gRPC code from the protobuf definitions
-python -m grpc_tools.protoc -I/app/client --python_out=/app/client --grpc_python_out=/app/client /app/client/workloads.proto
+python -m grpc_tools.protoc -I/app/addon --python_out=/app/addon --grpc_python_out=/app/addon /app/addon/workloads.proto
 echo "generated protobuf files"
 
 # Start the main application
-echo "starting the client"
-python /app/client/main.py
+echo "starting the addon"
+python /app/addon/main.py
